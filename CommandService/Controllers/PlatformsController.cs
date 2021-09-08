8a5f738d99f7a55ef3dedc0888197fba80aa0eca
Nestor Matias <solomatias321@gmail.com>
2021-09-08 11:36:02 +0200

    adding controller to receive data from platform service

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> InBound Post # Command Service");
+
+            return Ok("Inbound test from Platforms Controller");
+        }
+
+    }
+}
