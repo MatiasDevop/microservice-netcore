aa21f244649a75145d1f4273a2d6e5e54186a4ca
Nestor Matias <solomatias321@gmail.com>
2021-09-17 10:33:59 +0200

    adding controller api and dtos

---
+++ CommandService/Controllers/CommandsController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            Console.WriteLine($"--> hit GetCommandsFor Platform :{platformId}");
+            if(!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var commands = _repository.GetCommandsForPlatform(platformId);
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+    }
+}
--- CommandService/Controllers/PlatformsController.cs
+++ CommandService/Controllers/PlatformsController.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandService.Controllers
@@ -7,10 +11,26 @@ namespace CommandService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepository _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepository repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting platforms from Command Service");
+
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+
+        }
+
 
         [HttpPost]
         public ActionResult TestInboundConnection()
